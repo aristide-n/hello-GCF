93e56ac9bb247a77753032de796b45e56e0ca2fe
Aristide Niyungeko <2230766+aristide-n@users.noreply.github.com>
2019-12-16 14:52:24 -0800

    contacts list screen (#2) pt 2

--- src/routes/contacts/index.js
+++ src/routes/contacts/index.js
@@ -1,11 +1,27 @@
 import { h, Component } from 'preact';
+import List from 'preact-material-components/List';
+import 'preact-material-components/List/style.css';
 import style from './style.css';
 
 export default class ContactsList extends Component {
 	render() {
+		const contacts = ['Bob Ford', 'Alice Boxer', 'Mallory Malek', 'Eve Potter'];
 		return (
-			<div class={`${style.foo} page`}>
-				Contacts
+			<div class={`${style.contactsList} page`}>
+				<List avatar-list>
+					{contacts.map(contactName => {
+						return (
+							<List.Item>
+								<List.ItemGraphic>perm_identity</List.ItemGraphic>
+								<List.TextContainer>{contactName}</List.TextContainer>
+							</List.Item>
+						);
+					})}
+					<List.Item>
+						<List.ItemGraphic>add_circle_outline</List.ItemGraphic>
+						<List.TextContainer>Add a contact</List.TextContainer>
+					</List.Item>
+				</List>
 			</div>
 		);
 	}
