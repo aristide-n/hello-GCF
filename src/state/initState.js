31cd3dcdf13f50d1d2e0457ba4d3a9711f7237a8
Aristide Niyungeko <2230766+aristide-n@users.noreply.github.com>
2020-03-03 22:40:03 -0800

    fix bugs

--- src/state/initState.js
+++ src/state/initState.js
@@ -1,31 +1,45 @@
 import firebase, { firestore } from './firebase';
 
 const initState = contactStore => {
+	// bug? - users sometimes see other users' invitations (e.g. when one user logs in after another and sends the last
+	// user an invitation). It may have been fixed side effectedly.
+	if (contactStore.outgoingInvitations.size > 0)
+		contactStore.outgoingInvitations = new Map();
+	if (contactStore.incomingInvitations.size > 0)
+		contactStore.incomingInvitations = new Map();
 	const currentUserRef = firestore.collection('users').doc(firebase.auth().currentUser.uid);
 	currentUserRef.collection('outgoing_invitations').onSnapshot(snapshot => {
 			snapshot.docChanges().forEach((docChange) => {
 				switch (docChange.type) {
 					case 'added':
-					case 'modified':
-						// todo - this feels error prone, look for bugs and see if I can refactor it
 						if (docChange.doc.data().isAccepted || docChange.doc.data().isDeclined) {
-							// delete outgoing invite doc
-							docChange.doc.ref.delete()
-								.catch(err => console.error('Error deleting outgoing_invitations doc: ', err));
 							if (docChange.doc.data().isAccepted) {
 								console.log('adding contact: ', docChange.doc.data().contactRef);
 								// create new contact doc
 								currentUserRef.collection('contacts').doc()
 									// todo: use the email from the latest snapshot, !from the outgoing_invitations doc?
-									// todo: fix bug 1 - multiple contact docs created with the same data and some extra weirdness
-									// bug 2 - users sometimes see other users' invitations (e.g. when one user logs in after
-									// another and sends the last user an invitation)
 									.set({ userRef: docChange.doc.data().contactRef, email: docChange.doc.data().email})
 									.catch(err => console.error('Error adding contact: ', err));
 							}
+							// delete outgoing invite doc
+							docChange.doc.ref.delete()
+								.catch(err => console.error('Error deleting outgoing_invitations doc: ', err));
 						} else {
 							contactStore.addOutgoingInvitation(docChange.doc.id, docChange.doc.data());
 						}
+						break;
+					case 'modified':
+						if (docChange.doc.data().isAccepted) {
+							console.log('adding contact: ', docChange.doc.data().contactRef);
+							// create new contact doc
+							currentUserRef.collection('contacts').doc()
+								// todo: use the email from the latest snapshot, !from the outgoing_invitations doc?
+								.set({ userRef: docChange.doc.data().contactRef, email: docChange.doc.data().email})
+								.catch(err => console.error('Error adding contact: ', err));
+						}
+						// delete outgoing invite doc
+						docChange.doc.ref.delete()
+							.catch(err => console.error('Error deleting outgoing_invitations doc: ', err));
 						break;
 					case 'removed':
 						contactStore.removeOutgoingInvitation(docChange.doc.id);
